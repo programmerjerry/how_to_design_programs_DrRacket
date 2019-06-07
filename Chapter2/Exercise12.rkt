2ceb66719bf10e03c004eaad1e24c0ec27d2a14f
programmerjerry <me@programmerjerry.com>
2019-06-07 10:54:12 +0800

    完成 Exercise12

---
+++ Chapter2/Exercise12.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise12) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+(define (cvolume x y z)
+  (* x y z))
+
+(define (csurface x y z)
+  (* (+ (* x y)
+        (* y z)
+        (* z x))
+     2))
