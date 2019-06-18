70c8f9e88109da0c8832e8e17b2f46dc4bca293a
programmerjerry <me@programmerjerry.com>
2019-06-18 21:10:28 +0800

    完成 Exercise72

---
+++ Chapter5/Exercise72.rkt
@@ -0,0 +1,36 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise72) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+(define-struct phone# [area switch num])
+
+
+(define MTS (empty-scene 100 100))
+(define DOT (circle 3 "solid" "red"))
+
+;(define-struct posn [x y])
+
+(check-expect (scene+dot (make-posn 10 20))
+              (place-image DOT 10 20 MTS))
+(check-expect (scene+dot (make-posn 88 73))
+              (place-image DOT 88 73 MTS))
+
+
+(define (scene+dot p)
+  (place-image DOT (posn-x p) (posn-y p) MTS))
+
+; Posn -> Posn
+; increases the x-coordinate of p by 3
+(check-expect (x+ (make-posn 10 0)) (make-posn 13 0))
+
+(define (x+ p)
+  (make-posn (+ (posn-x p) 3) (posn-y p)))
+
+; A Posn represents the state of the world.
+ 
+; Posn -> Posn 
+(define (main p0)
+  (big-bang p0
+    [on-tick x+]
+    [on-mouse reset-dot]
+    [to-draw scene+dot]))
+
