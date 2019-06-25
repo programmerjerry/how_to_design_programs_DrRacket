551fa6b62c745a446c7d271829c3cf6fd8c4e218
programmerjerry <me@programmerjerry.com>
2019-06-25 22:01:05 +0800

    完成 Exercise86

---
+++ Chapter5/Exercise86.rkt
@@ -0,0 +1,82 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise86) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+(define-struct editor [pre post]) 
+; An Editor is a structure: 
+;    (make-editor String String) 
+; interpretation (make-editor s t) describes an editor 
+; whose visible text is (string-append s t) with 
+; the cursor displayed between s and t
+
+(define editor2 (make-editor "hello " "world"))
+
+(define MAX-EDITOR-LENGTH 24)
+
+(define (string-rest str)
+  (if (string=? str "")
+      ""
+      (substring str
+                 1
+                 (string-length str))))
+
+
+(define (string-first str)
+  (if (string=? str "")
+      ""
+      (substring str 0 1)))
+
+(define (string-last str)
+  (if (string=? str "")
+      ""
+      (substring str (- (string-length str)
+                        1)
+                 (string-length str))))
+
+(define (string-remove-last str)
+  (if (string=? str "")
+      ""
+      (substring str
+                 0
+                 (- (string-length str)
+                    1))))
+
+
+(define (render editor1)
+  (overlay/align "left" "center"
+                 (beside      (text (editor-pre editor1) 16 "black")
+                              (rectangle 1 20 "solid" "red")
+                              (text (editor-post editor1) 16 "black"))           
+                 (empty-scene 200 20)))
+
+(define (edit ed ke)
+  (if (> (string-length ke) 1)
+      (cond
+        [(key=? "left" ke)
+         (make-editor (string-remove-last (editor-pre ed))
+                      (string-append (string-last (editor-pre ed))
+                                     (editor-post ed)))]
+      
+        [(key=? "right" ke)
+         (make-editor
+          (string-append (editor-pre ed)
+                         (string-first (editor-post ed)))
+          (string-rest (editor-post ed)))]
+        [else ed])
+      (cond
+        [(key=? "\b" ke)
+         (make-editor (string-remove-last (editor-pre ed))
+                      (editor-post ed))]
+        [(or (key=? "\t" ke)
+             (key=? "\r" ke)) ed]
+        [else
+         (if (> (string-length (string-append (editor-pre ed)
+                                              (editor-post ed)))
+                MAX-EDITOR-LENGTH)
+             ed
+             (make-editor (string-append (editor-pre ed) ke)
+                          (editor-post ed)))])))
+
+(define (main ed)
+  (big-bang ed
+    [to-draw render]
+    [on-key edit]))
