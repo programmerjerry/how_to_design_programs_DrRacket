edd389f92516d5ea49e6fb76133859f65e40542d
programmerjerry <me@programmerjerry.com>
2019-06-27 17:39:45 +0800

    完成 Exercise95

---
+++ Chapter6/Exercise95.rkt
@@ -0,0 +1,4 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise95) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+导弹发射后，水平位置不会变化，垂直位置有变动，如果想要命中UFO，只能是根据坦克或者UFO的位置生成数据。
