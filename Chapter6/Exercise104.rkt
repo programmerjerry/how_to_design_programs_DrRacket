01005477ea9a7826ada43257c7a1cc273142c6bf
programmerjerry <me@programmerjerry.com>
2019-07-19 16:12:40 +0800

    完成 Exercise104

---
+++ Chapter6/Exercise104.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise104) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+;The representation of each vehicle must describe the number of passengers that it can carry, its license plate number,
+;and its fuel consumption (miles per gallon). Develop a template for functions that consume vehicles.
+(define-struct vehicles [number-of-passengers license-plate-number fuel-consumption])
